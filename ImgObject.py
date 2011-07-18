bb7fc9d2b567c09b1df14ccdb05f3294c82ea5e8
katsu <oggyist@gmail.com>
2011-07-18 07:50:40 +0000

    size classが、まだ中にある。が、ほぼphp通り。
    git-svn-id: file:///home/katsu/svn/WallPosit/trunk@5 ddb5f4a8-44ec-4cfb-82cf-38f0ca48b327

--- ImgObject.py
+++ ImgObject.py
@@ -3,6 +3,7 @@
 #coding: UTF-8
 
 from PIL import Image
+import math
 from var_dump import var_dump
 
 class ImgObject(Image.Image):
@@ -22,11 +23,35 @@ class ImgObject(Image.Image):
 	def getGeometry(self):
 		return self.size
 
+	def isSquare(self):
+		widthAs = 4;
+		heightAs = 3;
+		return self.checkAspectRatio(widthAs, heightAs)
+
+	def isWide(self):
+		widthAs = 16;
+		heightAs = 9;
+		return self.checkAspectRatio(widthAs, heightAs)
+
+	def isDual(self):
+		widthAs = 8;
+		heightAs = 2.7;
+		return self.checkAspectRatio(widthAs, heightAs)
+
+	def checkAspectRatio(self, widthAs, heightAs):
+		quotient_w = math.floor(self.size.w / widthAs)
+		quotient_h = math.floor(self.size.h / heightAs)		
+		if (quotient_w == quotient_h):
+			return str(widthAs) + ':' + str(heightAs)
+		else:
+			return 'No Match: ' + str(quotient_w) + ':' + str(quotient_h)
 
 #///////////////////////////////////////////////////////////////////////////////////// main
 
 if __name__ == "__main__":
-	file = '/home/katsu/Wallpaper/transformers2_00101.jpg'
+	file = '1500x844.jpg'	#16:9
+#	file = '1000x800.jpg'	#No Match
+#	file = '2560x1920.jpg' #4:3
 
 	imgObj = ImgObject(Image.open(file))
 
@@ -34,3 +59,8 @@ if __name__ == "__main__":
 
 	print imgObj.getGeometry().w
 	print imgObj.getGeometry().h
+
+	print imgObj.isSquare()
+	print imgObj.isWide()
+	print imgObj.isDual()
+
