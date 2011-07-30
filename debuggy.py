503c242d0613cfdc367f972e3592e2446e17b66d
katsu <oggyist@gmail.com>
2011-07-30 04:29:27 +0000

    find_names()は、中断。一時保存。
    git-svn-id: file:///home/katsu/svn/WallPosit/trunk@12 ddb5f4a8-44ec-4cfb-82cf-38f0ca48b327

--- debuggy.py
+++ debuggy.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python
 
-#coding: UTF-8
+# -*- coding: utf-8 -*-
 
 import datetime
+import re
 #import locale
 
-def debugCon (val):
+def dgConv(val):
+	ptn = re.compile(r'^<type\s\'(.+)\'>$')
+	subStr = ptn.search( str( type(val) ) )
+	tp = subStr.group(1) 
+	if tp == 'int' or tp == 'float' or tp == 'double':
+		return str(val)
+	elif tp == 'Instance':
+		pass
+	else:
+		return str(val)
+
+def dgLine(val):
+	print dgConv(val)
+
+def dgLog(val):
 	td = datetime.datetime.now()
-	print td.strftime("%Y/%m/%d %H:%M:%S") + ' [debug] :' + str(val) + '.\n',
+	print td.strftime("%Y/%m/%d %H:%M:%S") + ' [debug] :' + dgConv(val) + '.\n',
+
 
 if __name__ == "__main__":
-	debugCon("hoge")
+	dgLine('fuga')
+	dgLine(1)
+	dgLine(0.001)
+	n = 300
+	dgLine(n)
+	l = 300
+	dgLine(l)
+	dgLog(0.002)
