cb1ab3c8814b80320769109b9de6a2aa5ba3c99c
katsu <oggyist@gmail.com>
2011-10-23 15:20:55 +0000

    パッケージング: 	__init__.pyに、__all__を記述
    git-svn-id: file:///home/katsu/svn/WallPosit/trunk@45 ddb5f4a8-44ec-4cfb-82cf-38f0ca48b327

---
+++ WallpaperOptimizer/Imaging/__init__.py
@@ -0,0 +1,2 @@
+# module WallpaperOptimizer.Imaging __init__.py
+__all__=["WoBounds","WoImgFile","WoRectangle","WoWorkSpace"]
---
+++ WallpaperOptimizer/__init__.py
@@ -0,0 +1,2 @@
+# module WallpaperOptimizer __init__.py
+__all__=["WoConfig","WoOption","debuggy"]
