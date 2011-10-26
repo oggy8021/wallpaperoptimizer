d824f8133616a942f4ee182067c594b8813a261f
katsu <oggyist@gmail.com>
2011-10-26 14:47:49 +0000

    WoChangerDir: 	初期登録。globはやめてos.listdir()と拡張子マッチング
    git-svn-id: file:///home/katsu/svn/WallPosit/trunk@47 ddb5f4a8-44ec-4cfb-82cf-38f0ca48b327

---
+++ WallpaperOptimizer/WoChangerDir.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import random
+import os, os.path, re
+
+class WoChangerDir(object):
+
+	def getImgfileSeq(self):
+		if (self.maxlen <= self.fp):
+			self.fp = 0
+		imgfile = self.imgfiles[self.fp]
+		self.fp += 1
+		return imgfile
+
+	def getImgfileRnd(self):
+		return self.imgfiles[int(random.randint(0,self.maxlen-1))]
+
+	def __init__(self, srcdir='.'):
+		self.fp = 0
+
+		Ext = re.compile("\.(gif|jpe?g|bmp|png)$", re.IGNORECASE)
+		files = os.listdir(srcdir)
+		self.imgfiles = []
+		for file in files:
+			file
+			if (os.path.isfile(srcdir + file) and Ext.search(file)):
+				self.imgfiles.append(os.path.abspath(srcdir + file))
+		self.maxlen = len(self.imgfiles)
+
+if __name__ == "__main__":
+	import sys
+	wChg = WoChangerDir(sys.argv[1])
+	for i in range (0,int(sys.argv[2])):
+		print i, wChg.getImgfileSeq()
+	print '---'
+	for i in range (0,int(sys.argv[2])):
+		print i, wChg.getImgfileRnd()
